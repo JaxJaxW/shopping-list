ddd478c944b202887b94409d55a5f5279d71c493
Jaxton Willman <jaxtonwillman@gmail.com>
2018-12-10 17:37:11 -0500

    To PC

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const electron = require('electron');
 const url = require('url');
 const path = require('path');
 
-const {app, BrowserWindow, Menu, ipcMain} = electron;
+const { app, BrowserWindow, Menu, ipcMain } = electron;
 
 // SET ENV
 process.env.NODE_ENV = 'production';
@@ -11,7 +11,7 @@ let mainWindow;
 let addWindow;
 
 // Listen for app to be ready
-app.on('ready', function(){
+app.on('ready', function() {
     // Create new window
     mainWindow = new BrowserWindow();
     // Load HTML into window
@@ -21,7 +21,7 @@ app.on('ready', function(){
       slashes: true
     }));
     // Quit app when closed
-    mainWindow.on('closed', function(){
+    mainWindow.on('closed', function() {
         app.quit();
     });
 
@@ -34,56 +34,52 @@ app.on('ready', function(){
 // Handle create add window
 function createAddWindow(){
     // Create new window
-    addWindow = new BrowserWindow({
+    addWindow = new BrowserWindow( {
         width: 300,
         height: 200,
         title: 'Add Shopping List Item'
     });
     // Load HTML into window
-    addWindow.loadURL(url.format({
+    addWindow.loadURL(url.format( {
       pathname: path.join(__dirname, 'addWindow.html'),
       protocol: 'file:',
       slashes: true
     }));
     // Garbage Collection handle
-    addWindow.on('close', function(){
+    addWindow.on('close', function() {
         addWindow = null;
     });
 }
 
 // Catch item:add
-ipcMain.on('item:add', function(e, item){
+ipcMain.on('item:add', function(e, item) {
     mainWindow.webContents.send('item:add', item);
     addWindow.close();
-
 });
 
 // Create menu template
-const mainMenuTemplate =[
+const mainMenuTemplate = [
   {
-    label: 'File',
-    submenu:[
-      {
-          label: 'Add Item',
-          click() {
-              createAddWindow();
-          }
-        },
-        {
-          label: 'Clear Items',
-          click() {
-              mainWindow.webContents.send('item:clear');
-          }
-        },
-        {
-          label: 'Quit',
-          accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
-          click() {
-            app.quit();
-          }
-        }
-        ]
+    label: 'Add Item',
+    accelerator: process.platform == 'darwin' ? 'Command+N' : 'Ctrl+N',
+    click() {
+        createAddWindow();
     }
+  },
+  {
+    label: 'Clear Items',
+    accelerator: process.platform == 'darwin' ? 'Command+C' : 'Ctrl+C',
+    click() {
+        mainWindow.webContents.send('item:clear');
+    }
+  },
+  {
+    label: 'Quit',
+    accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+    click() {
+        app.quit();
+    }
+  }
 ];
 
 // If mac, add empty object to menu
